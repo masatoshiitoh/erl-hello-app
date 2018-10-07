2064fd0e6f9733ee46e9d5b5ae47fe8d0eaf1060
Masatoshi Itoh <masatoshi9953@gmail.com>
2018-10-08 00:23:12 +0900

    fixed: gcloud auth for docker

--- 0-login-gcp.sh
+++ 0-login-gcp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcloud auth application-default login
 gcloud auth configure-docker
+gcloud auth application-default login
 # gcloud auth login
 export PROJECT_ID=`gcloud config get-value project`
 
--- 1-build-container-image.sh
+++ 1-build-container-image.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+gcloud docker --authorize-only
 docker build -t gcr.io/${PROJECT_ID}/erl-hello-app:v1 .
